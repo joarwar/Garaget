ae5838d2bd16a532eca930366cb5c764e21ae951
NiklasLindblad <niklas.o.lindblad@gmail.com>
2021-10-05 08:27:24 +0200

    Updated garageclass to have uint (since we can't have negative amounts of parking lots) This required a change in the garage creation menu

--- Garaget/Garage.cs
+++ Garaget/Garage.cs
@@ -1,17 +1,53 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Garaget
 {
-    class Garage
+    //interface IEnumerator<T>
+    //{
+    //    void ListVehicles(T item);
+    //    void AddVehicle(T item);
+    //}
+
+    class Garage <T> : IEnumerable<T> where T : Vehicles
     {
-        public int ParkingSpots { get; set; }
-        public Garage(int parkingSpots)
+        public uint ParkingSpots { get; private set; }
+        private List<T> _vehicles = new List<T>();
+
+        public void AddVehicle(T item)
+        {
+            _vehicles.Add(item);
+        }
+
+        public T this[int idx] => _vehicles[idx]; // Indexer - Used for returning a value based on a int index
+        public int Count => _vehicles.Count;
+
+        public Garage(uint parkingSpots)
         {
             ParkingSpots = parkingSpots;
+        }
+
+        // returning the Enumerator of the List _vehicles.
+        // In order to use this,
+        /*
+         * Instantiate IEnumerator<Vehicles> vehi = garaget.GetEnumerator();
+         * while()
+         */
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _vehicles.GetEnumerator();
+        }
+
+        // Legacy implementation
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        // Legacy implementation
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
