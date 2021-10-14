8b83aade92639f2b659043ee73b5880881859b34
NiklasLindblad <niklas.o.lindblad@gmail.com>
2021-10-14 13:36:56 +0200

    changed logic for exit menu so it happens when there is no new menu to show

--- Garaget/ExitMenu.cs
+++ Garaget/ExitMenu.cs
@@ -4,25 +4,14 @@ namespace Garaget
 {
     class ExitMenu : Menu
     {
-        //public int AmountOfMenuItems { get; set; }
-        //public Menu NextMenu { get; set; }
-        ////tänka om
-        //public ExitMenu()
-        //{
-        //    AmountOfMenuItems = 1;
-        //}
-
         public override Menu ShowMenu()
         {
-            Console.WriteLine("Thank you for using our garage");
-            HandleInput();
+            Console.WriteLine("Thank you for using our garage. The garage has been saved.");
             return null;
         }
 
         public override bool HandleInput()
         {
-            Program.garage.SaveState(Program.path);
-            Environment.Exit(0);
             return false;
         }
 
