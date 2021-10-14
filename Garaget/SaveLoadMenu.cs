cc86932e862128918d1ee720edf3c528a21a6c5a
NiklasLindblad <niklas.o.lindblad@gmail.com>
2021-10-14 13:21:10 +0200

    Removed unneeded Console.ReadLine() which made interaction strange

--- Garaget/SaveLoadMenu.cs
+++ Garaget/SaveLoadMenu.cs
@@ -28,7 +28,6 @@ namespace Garaget
                     Console.WriteLine("Try again!");
                     break;
             }
-            Console.ReadLine();
             return false;
         }
 
